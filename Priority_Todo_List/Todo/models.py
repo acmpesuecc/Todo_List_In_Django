1408b9e5a70fcaa3a39da5f433408f3bacb0344a
anirudhRowjee <ani.rowjee@gmail.com>
2020-10-24 19:15:34 +0530

    Fixes #8
    Hi,
    this commit fixes issue #8.
    
    Changelog -
    1. Modified `models.py` per requirement.

--- Priority_Todo_List/Todo/models.py
+++ Priority_Todo_List/Todo/models.py
@@ -5,17 +5,17 @@ from datetime import datetime
 # Create your models here.
 class Todo(models.Model):
     # Todo create a title attribute -> char
-    title = ''
+    title = models.CharField(max_length=255)
 
     # Todo create a content attribute -> text
-    content = ''
+    content = models.CharField(max_length=1000)
 
     # Todo create a created_at attribute -> timestamp
-    created_at = ''
+    created_at = models.DateTimeField(auto_now_add=True)
 
     # Todo create priority attribute -> integer
-    priority = ''
+    priority = models.IntegerField(default=1)
 
     def __str__(self):
         # Todo return title
-        return ''
+        return f"{self.title}"
